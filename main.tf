291cb623ad13d406ba9454a2f9841d9316a7a33f
Nick Mahoney <nickmahoney@thisisartium.com>
2023-04-16 20:37:45 -0700

    Copy Terraform ECS example
    Copied from https://github.com/terraform-aws-modules/terraform-aws-ecs/tree/v3.1.0/examples/complete-ecs

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.52.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
   }
   required_version = ">= 1.1.0"
 
@@ -24,56 +20,145 @@ provider "aws" {
   region = var.region
 }
 
-resource "random_pet" "sg" {}
+################################################################################
+# Copied from https://github.com/terraform-aws-modules/terraform-aws-ecs/blob/v3.1.0/examples/complete-ecs/main.tf
 
-data "aws_ami" "ubuntu" {
+locals {
+  name        = "gha-tf-aws-ecs-example"
+  environment = "dev"
+
+  # This is the convention we use to know what belongs to each other
+  ec2_resources_name = "${local.name}-${local.environment}"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+
+  cidr = "10.1.0.0/16"
+
+  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.11.0/24", "10.1.12.0/24"]
+
+  enable_nat_gateway = false # false is just faster
+
+  tags = {
+    Environment = local.environment
+    Name        = local.name
+  }
+}
+
+#----- ECS --------
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+
+  name               = local.name
+  container_insights = true
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT", aws_ecs_capacity_provider.prov1.name]
+
+  default_capacity_provider_strategy = [{
+    capacity_provider = aws_ecs_capacity_provider.prov1.name # "FARGATE_SPOT"
+    weight            = "1"
+  }]
+
+  tags = {
+    Environment = local.environment
+  }
+}
+
+module "ec2_profile" {
+  source = "terraform-aws-modules/ecs/aws//modules/ecs-instance-profile"
+
+  name = local.name
+
+  tags = {
+    Environment = local.environment
+  }
+}
+
+resource "aws_ecs_capacity_provider" "prov1" {
+  name = "prov1"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = module.asg.autoscaling_group_arn
+  }
+
+}
+
+#----- ECS  Services--------
+module "hello_world" {
+  source = "./service-hello-world"
+
+  cluster_id = module.ecs.ecs_cluster_id
+}
+
+#----- ECS  Resources--------
+
+#For now we only use the AWS ECS optimized ami <https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html>
+data "aws_ami" "amazon_linux_ecs" {
   most_recent = true
+
+  owners = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    name   = "owner-alias"
+    values = ["amazon"]
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "~> 4.0"
+
+  name = local.ec2_resources_name
+
+  # Launch configuration
+  lc_name   = local.ec2_resources_name
+  use_lc    = true
+  create_lc = true
+
+  image_id                 = data.aws_ami.amazon_linux_ecs.id
+  instance_type            = "t2.micro"
+  security_groups          = [module.vpc.default_security_group_id]
+  iam_instance_profile_arn = module.ec2_profile.iam_instance_profile_arn
+  user_data = templatefile(
+    "${path.module}/templates/user_data.sh.tpl",
+    { cluster_name = local.name },
+  )
+
+  # Auto scaling group
+  vpc_zone_identifier       = module.vpc.private_subnets
+  health_check_type         = "EC2"
+  min_size                  = 0
+  max_size                  = 2
+  desired_capacity          = 2
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = local.environment
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Cluster"
+      value               = local.name
+      propagate_at_launch = true
+    },
+  ]
 }
+
+# End of section copied from https://github.com/terraform-aws-modules/terraform-aws-ecs/blob/v3.1.0/examples/complete-ecs/main.tf
