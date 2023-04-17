50ad72b5c6aa4bcce109b863eb3949244fc8b560
Nick Mahoney <nickmahoney@thisisartium.com>
2023-04-16 21:59:31 -0700

    Fix reference to region variable

--- main.tf
+++ main.tf
@@ -193,7 +193,7 @@ module "vpc" {
   name = local.name
   cidr = "10.99.0.0/18"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
   public_subnets  = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
   private_subnets = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
 
